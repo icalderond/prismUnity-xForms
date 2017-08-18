cae03d43427b0ead7c551114984d127df3ee7d6d
icalderond <icalderond@outlook.com>
2017-08-18 12:44:25 -0500

    Aplicando navegacion al viewModel

--- prismXForms/ViewModels/MainPageViewModel.cs
+++ prismXForms/ViewModels/MainPageViewModel.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using Prism.Mvvm;
+using Prism.Navigation;
 using Xamarin.Forms;
 
 namespace prismXForms.ViewModels
 {
-    public class MainPageViewModel : BindableBase
+    public class MainPageViewModel : BindableBase, INavigationAware
     {
-        public MainPageViewModel()
-        {
+        private INavigationService _navigationService;
 
+        public MainPageViewModel(INavigationService navigationService) // has to be named correctly
+        {
+            _navigationService = navigationService;
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
         }
     }
 }
--- prismXForms/ViewModels/SecondPageViewModel.cs
+++ prismXForms/ViewModels/SecondPageViewModel.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using Prism.Mvvm;
+using Prism.Navigation;
 
 namespace prismXForms.ViewModels
 {
-    public class SecondPageViewModel : BindableBase
+    public class SecondPageViewModel : BindableBase, INavigationAware
     {
-        public SecondPageViewModel()
+        private INavigationService _navigationService;
+
+        public SecondPageViewModel(INavigationService navigationService) // has to be named correctly
         {
+            _navigationService = navigationService;
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
         }
     }
 }
